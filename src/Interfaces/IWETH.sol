cd8a940da8f03f58a6d353a7271cc2fc852d1b69
c-note <dongustavo20@gmail.com>
2023-09-27 14:22:47 +0100

    Add import file.

--- src/Interfaces/IWETH.sol
+++ src/Interfaces/IWETH.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-interface IWETH {
+import "./IERC20.sol";
+
+interface IWETH is IERC20 {
     function deposit() external payable;
 
     function withdraw(uint amount) external;
